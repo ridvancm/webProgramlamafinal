8f551b7e3f831ba531a1ab7651b3d6515c475515
Nasuh Mert Ünal <unalnasuhmert@gmail.com>
2024-12-26 16:31:03 +0300

    guncelleme

---
+++ webProgramlamafinal/Models/KullaniciTarifi.cs
@@ -0,0 +1,11 @@
+﻿namespace WebProgramlamaFinal.Models
+{
+    public class KullaniciTarifi
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Malzemeler { get; set; }
+        public string Tarif { get; set; }
+        public string ResimUrl { get; set; }
+    }
+}
